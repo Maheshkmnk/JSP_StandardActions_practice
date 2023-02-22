39ff30f0c043bbda53744aa717640ae62320dabe
Maheshkmnk <68604040+Maheshkmnk@users.noreply.github.com>
2023-02-22 23:12:57 +0530

    JSP Standard actions third commit

---
+++ JspStandardAction3/src/main/java/in/mypackage/service/SalaryDetailsGenerator.java
@@ -0,0 +1,7 @@
+package in.mypackage.service;
+
+import in.mypackage.dto.EmployeeDto;
+
+public interface SalaryDetailsGenerator {
+	public void generateSalaryDetails(EmployeeDto dto);
+}
---
+++ JspStandardAction3/src/main/java/in/mypackage/service/SalaryDetailsGeneratorImpl.java
@@ -0,0 +1,27 @@
+package in.mypackage.service;
+
+
+import in.mypackage.dto.EmployeeDto;
+
+public class SalaryDetailsGeneratorImpl implements SalaryDetailsGenerator {
+	
+	static {
+		System.out.println("SalaryDetailGeneratorImpl.class file is loading...");
+	}
+
+	public SalaryDetailsGeneratorImpl() {
+		System.out.println("SalaryDetailsGeneratorImpl object is created...");
+	}
+	
+	@Override
+	public void generateSalaryDetails(EmployeeDto dto) {
+		// generate grossSalary and netSalary
+		float grossSalary = dto.getBsalary() + (dto.getBsalary() * 0.3f);
+		float netSalary = grossSalary - (dto.getBsalary() * 0.2f);
+
+		// set those values into DTO
+		dto.setGrossSalary(grossSalary);
+		dto.setNetSalary(netSalary);
+
+	}
+}
