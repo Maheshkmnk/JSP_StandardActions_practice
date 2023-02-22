741b3bc427f0982d8a61729ff3ed4d89af534639
Maheshkmnk <68604040+Maheshkmnk@users.noreply.github.com>
2023-02-22 23:06:11 +0530

    Jsp Standard actions practice 1st code

---
+++ JspStandardAction/src/main/java/in/mypackage/bean/Calculator.java
@@ -0,0 +1,16 @@
+package in.mypackage.bean;
+
+public class Calculator {
+	static {
+		System.out.println("Calculator.class file is loading...");
+	}
+
+	public Calculator() {
+		System.out.println("Calculator class object is created...");
+	}
+
+	public int squareIt(int x) {
+		return x * x;
+	}
+}
+
